62f427df4192848927a6e9d70369bef80e102398
Soroush Khosh <sur.khosh@outlook.com>
2023-03-11 22:21:57 +0330

    76

--- react/pics/src/App.js
+++ react/pics/src/App.js
@@ -1,18 +1,23 @@
+import { useState } from "react";
 import SearchBar from "./components/SearchBar";
+import ImageList from "./components/ImageList";
 import searchImages from "./api";
 
 function App() {
-    const handleSubmit = async (term) => {
-      const result = await searchImages(term);
+  const [images, setImages] = useState([]);
 
-      console.log(result);
-    };
+  const handleSubmit = async (term) => {
+    const result = await searchImages(term);
+
+    setImages(result);
+  };
 
   return (
     <div>
       <SearchBar onSubmit={handleSubmit} />
+      <ImageList images={images} />
     </div>
   );
-}
+} 
 
 export default App;
