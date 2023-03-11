c3aa914560e167a1b1a665007cc75b1889a459ea
Soroush Khosh <sur.khosh@outlook.com>
2023-03-11 08:16:27 +0330

    69

--- react/pics/src/components/SearchBar.js
+++ react/pics/src/components/SearchBar.js
@@ -1,5 +1,14 @@
-function SearchBar() {
-    return <div>SearchBar</div>;
+function SearchBar({ onSubmit }) {
+  const handleClick = () => {
+    onSubmit("cars");
+  };
+
+  return (
+    <div>
+      <input />
+      <button onClick={handleClick}>Click this</button>
+    </div>
+  );
 }
 
 export default SearchBar;
