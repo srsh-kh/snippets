b04f53a55dd2b83f26e6631c4878915074d375f7
Soroush Khosh <sur.khosh@outlook.com>
2023-03-13 17:31:19 +0330

    x

--- react/pics/src/api.js
+++ react/pics/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const searchImages = async (term) => {
   const response = await axios.get("https://api.unsplash.com/search/photos", {
     headers: {
-      Authorization: "Client-ID n5tVW9Q7QK-u-BjKdbqRv6OHGGyJeicfaV4LBRdYTtI",
+      Authorization: "Client-ID 66n5tVW9Q7QK-u-BjKdbqRv6OHGGyJeicfaV4LBRdYTtI47",
     },
     params: {
         query: term
